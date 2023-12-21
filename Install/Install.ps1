569dcbdd87ba93d896d282ca6404312911cf6820
Tarjei E. Ormestøyl <ormestoyl@gmail.com>
2023-12-21 09:39:29 +0100

    Finalizing v1 of veiprosjekter

--- Install/Install.ps1
+++ Install/Install.ps1
@@ -1,10 +1,10 @@
 Param(
-    [Parameter(Mandatory = $true, HelpMessage = "URL to the Prosjektportalen hub site")]
-    [string]$Url,
-    [Parameter(Mandatory = $false, HelpMessage = "Skip search configuration")]
-    [switch]$SkipSearchConfiguration,
-    [Parameter(Mandatory = $false, HelpMessage = "Do you want to perform an upgrade?")]
-    [switch]$Upgrade
+  [Parameter(Mandatory = $true, HelpMessage = "URL to the Prosjektportalen hub site")]
+  [string]$Url,
+  [Parameter(Mandatory = $false, HelpMessage = "Skip search configuration")]
+  [switch]$SkipSearchConfiguration,
+  [Parameter(Mandatory = $false, HelpMessage = "Do you want to perform an upgrade?")]
+  [switch]$Upgrade
 )
 
 <#
@@ -35,6 +35,9 @@ $AdminSiteUrl = (@($Uri.Scheme, "://", $Uri.Authority) -join "").Replace(".share
 $TemplatesBasePath = "$PSScriptRoot/Templates"
 #endregion
 
+# TODO: Replace version from package.json/git-tag
+Write-Host "Installing Prosjektportalen veimodul version 1.0.0" -ForegroundColor Cyan
+
 #region Print installation user
 Connect-PnPOnline -Url $AdminSiteUrl -Interactive -ErrorAction Stop -WarningAction Ignore
 $CurrentUser = Get-PnPProperty -Property CurrentUser -ClientObject (Get-PnPContext).Web
@@ -62,4 +65,72 @@ StartAction("Applying veimodul template")
 Connect-PnPOnline -Url $Url -Interactive -ErrorAction Stop
 Invoke-PnPSiteTemplate -Path "$($TemplatesBasePath)/veimodul.pnp" -ErrorAction Stop
 EndAction
+#endregion
+
+#region Configure tillegg and standardinnhold
+StartAction("Configuring tillegg and standardinnhold")
+try {
+
+  Connect-PnPOnline -Url $Url -Interactive -ErrorAction Stop
+
+  $ListContent = Get-PnPListItem -List Listeinnhold
+  $Prosjekttillegg = Get-PnPListItem -List Prosjekttillegg
+  $Maloppsett = Get-PnPListItem -List Maloppsett
+  
+  $VeiTemplate = $Maloppsett | Where-Object { $_["Title"] -eq "Veiprosjekt" }
+  if ($null -ne $VeiTemplate) {
+    $VeiPlanner = $ListContent | Where-Object { $_["Title"] -eq "Planneroppgaver Vei" }
+    $VeiPhaseChecklist = $ListContent | Where-Object { $_["Title"] -eq "Fasesjekkpunkter Vei" }
+    $VeiDocuments = $ListContent | Where-Object { $_["Title"] -eq "Standarddokumenter Vei" }
+    $VeiItems = @()
+    $VeiItems += [Microsoft.SharePoint.Client.FieldLookupValue]@{"LookupId" = $VeiPlanner.Id }
+    $VeiItems += [Microsoft.SharePoint.Client.FieldLookupValue]@{"LookupId" = $VeiPhaseChecklist.Id }
+    $VeiItems += [Microsoft.SharePoint.Client.FieldLookupValue]@{"LookupId" = $VeiDocuments.Id }
+    $VeiTemplate["ListContentConfigLookup"] = $VeiItems
+      
+    $VeiTillegg = $Prosjekttillegg | Where-Object { $_["Title"] -eq "Veimal" }
+    $VeiTemplate["GtProjectExtensions"] = [Microsoft.SharePoint.Client.FieldLookupValue]@{"LookupId" = $VeiTillegg.Id }
+  
+    $VeiTemplate.SystemUpdate()
+    $VeiTemplate.Context.ExecuteQuery()
+  }
+  else {
+    Write-Host "[WARNING] Failed to find Veiprosjekt template. Please check the Maloppsett list." -ForegroundColor Yellow
+  }
+}
+catch {
+  EndAction
+  Write-Host "[WARNING] Failed to configure tillegg and standardinnhold: $($_.Exception.Message)" -ForegroundColor Yellow
+}
+EndAction
+#endregion
+
+#region Logging installation
+Write-Host "[INFO] Logging installation entry" 
+Connect-PnPOnline -Url $Url -Interactive -ErrorAction Stop
+$LastInstall = Get-PnPListItem -List "Installasjonslogg" -Query "<View><Query><OrderBy><FieldRef Name='Created' Ascending='False' /></OrderBy></Query></View>" | Select-Object -First 1 -Wait
+$PreviousVersion = "N/A"
+if ($null -ne $LastInstall) {
+  $PreviousVersion = $LastInstall.FieldValues["InstallVersion"]
+}
+# TODO: Replace version from package.json/git-tag
+$CustomizationInfo = "Prosjektportalen veimodul 1.0.0"
+$InstallStartTime = (Get-Date -Format o)
+$InstallEndTime = (Get-Date -Format o)
+
+$InstallEntry = @{
+  Title            = $CustomizationInfo;
+  InstallStartTime = $InstallStartTime; 
+  InstallEndTime   = $InstallEndTime; 
+  InstallVersion   = $PreviousVersion;
+  InstallCommand   = $MyInvocation.Line.Substring(2);
+}
+
+if ($null -ne $CurrentUser.Email) {
+  $InstallEntry.InstallUser = $CurrentUser.Email
+}
+
+## Logging installation to SharePoint list
+Add-PnPListItem -List "Installasjonslogg" -Values $InstallEntry -ErrorAction SilentlyContinue >$null 2>&1
+
 #endregion
--- README.md
+++ README.md
@@ -1,17 +1,27 @@
 # Veimodulen for Prosjektportalen
 
-Veimodulen for Prosjektportalen er en samling komponenter som sammen utgjør en mal for veiprosjekter. Etter installasjon 
+Veimodulen for Prosjektportalen er en samling komponenter som sammen utgjør en mal for veiprosjekter. Veimodulen er blitt utarbeidet av Rogaland fylkeskommune, og Puzzlepart har bistått i å ekstrahere tilpasningene som mal for deling på GitHub. Videre forvaltning vil gjøres primært av Puzzlepart, og vi ønsker innspill på innholdet i malen. For spørsmål og innspill, logg gjerne en issue i dette området på GitHub eller send oss en e-post på prosjektportalen@puzzlepart.com.
+
+Veimodulen installeres som et tillegg til Prosjektportalen 365. Ved å installere veimodulen vil man få følgende satt opp i porteføljeområdet
+
+1. En ny prosjektmal "Veiprosjekt" som man kan bruke som mal for nye prosjekter
+2. En ny fasesjekkliste "Fasesjekkliste Vei" med egne fasesjekkpunkter for veiprosjekter. Fasesjekklisten har også ny kolonne "Forankret i" for å indikere hvor fasesjekkpunktet er forankret
+3. En ny liste for planneroppgaver "Planneroppgaver Vei" med egne oppgaver for veiprosjekter
+4. Et nytt dokumentbibliotek "Standarddokumenter Vei" med egen folderstruktur som følger fasene
+5. Fasene i veiprosjekter er "Planlegge", "Prosjektere", "Bygge" og "Avslutte"
+6. Dokumentbiblioteket i veiprosjekter har fått to nye kolonner "Fag" og "Emne" (taksonomi)
+7. Nye prosjektegenskaper i veiprosjekter for ansvar i fasene
 
 ## Installasjon
 
-Denne pakken kommer ikke bundlet med PnP.PowerShell. Vi anbefaler sterkt å installere med samme versjon som kommer med Prosjektportalen 365, som per 12.12.2023 er PnP.PowerShell 1.11.0.
+Denne pakken kommer ikke bundlet med PnP.PowerShell. Vi anbefaler sterkt å installere med samme versjon som kommer med Prosjektportalen 365, som per 21.12.2023 er PnP.PowerShell 1.11.0.
 
-1. Pakk ut release-pakken lokalt
-2. Kjør Install.ps1 med -Url parameter til din Prosjektportalen-installasjon (må være installert på forhånd).
-3. Du kan nå opprette nye prosjekter og velge malen som heter "Veiprosjekt".
+1. Last ned release-pakken fra releases og pakk ut pakken lokalt
+2. Kjør Install.ps1 med -Url parameter til din Prosjektportalen-installasjon (PP365 må være installert på forhånd)
+3. Du kan nå opprette nye prosjekter og velge malen som heter "Veiprosjekt"
 
-# TODOS
+Eksempel: 
 
-TODO: Install: Logg oppføring i Installlogg
-TODO: Sjekk pre-installasjon hvorvidt RefinableStrings er ledig eller allerede allokert i tenanten
-TODO: Ferdigstill README
+``PowerShell
+.\Install.ps1 -Url https://puzzlepart.sharepoint.com/sites/ppveimodul
+``
