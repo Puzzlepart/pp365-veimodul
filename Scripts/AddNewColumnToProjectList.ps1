20ae013bec533d810ff3ceda38c436a82ad851a5
Remi Blom-Ohlsen <remi.blom-ohlsen@puzzlepart.com>
2025-10-07 12:12:15 +0200

    Add placeholder script

---
+++ Scripts/AddNewColumnToProjectList.ps1
@@ -0,0 +1,91 @@
+Param(
+    [string]$Url = "https://tenant.sharepoint.com/sites/site"
+)
+
+if ($null -eq (Get-Command Set-PnPTraceLog -ErrorAction SilentlyContinue)) {
+    Write-Host "Last inn PnP.PowerShell modulen før du kjører skriptet!" -ForegroundColor Yellow
+    exit 0
+}
+
+Start-Transcript -Path "$PSScriptRoot\Logs\FixExisting-$((Get-Date).ToString('yyyy-MM-dd-HH-mm')).txt"
+$ErrorActionPreference = "Stop"
+Set-PnPTraceLog -Off
+
+$Uri = [System.Uri]$Url
+$TenantAdminUrl = "https://" + $Uri.Authority.Replace(".sharepoint.com", "-admin.sharepoint.com")
+
+try {
+    Write-Host "Kobler til konfigurasjonsside for å hente liste over områder"
+    Connect-PnPOnline -Url $TenantAdminUrl -Interactive -ClientId da6c31a6-b557-4ac3-9994-7315da06ea3a
+
+    $ctx = Get-PnPContext
+    $ctx.Load($ctx.Web.CurrentUser)
+    $ctx.ExecuteQuery()
+    $CurrentUser = $ctx.Web.CurrentUser.LoginName
+
+    $HubSite = Get-PnPTenantSite -Identity $Url
+    $HubSiteId = $HubSite.HubSiteId.Guid
+    $AllSitesInHub = Get-PnPHubSiteChild -Identity $HubSiteId
+    $SitesToProcess = @()
+
+    if ($AllSitesInHub.length -gt 0) {
+        Write-Host "Prosesserer $($AllSitesInHub.length) områder"
+                
+        $i = 0
+
+        $AllSitesInHub | ForEach-Object {
+            $i = $i + 1
+            $TargetUrl = $_
+
+            $Percentage = [math]::Round(($i * 100) / $AllSitesInHub.length)
+            Write-Progress -Activity "Tilegner rettigheter for $CurrentUser på $($AllSitesInHub.length) områder" -Status "$Percentage% Ferdig" -PercentComplete $Percentage -CurrentOperation "Prosesserer $TargetUrl"
+
+            try {
+                $CurrentSite = Get-PnPTenantSite -Identity $TargetUrl
+                if ($CurrentSite.LockState -eq "Unlock") {
+                    Set-PnPTenantSite -Identity $TargetUrl -Owners $CurrentUser
+                    $SitesToProcess += $TargetUrl
+                }
+            }
+            catch {
+                Write-Host "`tEn feil oppsto ved oppdatering av område $TargetUrl" -ForegroundColor Red
+            }            
+        }
+        
+        Pause
+        $i = 0
+        $SitesToProcess | ForEach-Object {
+            $i = $i + 1
+            $TargetUrl = $_
+            Write-Host "Prosesserer $TargetUrl"
+
+            $Percentage = [math]::Round(($i * 100) / $SitesToProcess.length)
+            Write-Progress -Activity "Prosesserer $($SitesToProcess.length) områder" -Status "$Percentage% Ferdig" -PercentComplete $Percentage -CurrentOperation "Prosesserer $TargetUrl"
+
+            try {
+                Connect-PnPOnline -Url $TargetUrl -Interactive -ClientId da6c31a6-b557-4ac3-9994-7315da06ea3a
+
+                $List = Get-PnPList -Identity "XXX" -ErrorAction SilentlyContinue
+                if ($null -ne $List) {
+                    # DO STUFF
+                }
+                else {
+                    Write-Host "`tListen XXX finnes ikke på $TargetUrl" -ForegroundColor Yellow
+                }
+
+            }
+            catch {
+                Write-Host "`tFeilmelding: $($Error[0].Exception.Message)" -f Red
+            }
+        }
+    }
+    else {
+        Write-Host "Ingen områder å prosessere" -ForegroundColor Yellow
+    }
+}
+catch {
+    Write-Host "En uforventet feil oppsto: $($_.Exception.Message)" -ForegroundColor Red
+}
+finally {
+    Stop-Transcript
+}
